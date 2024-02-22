43f0173d7cdf3ba0090decf825267e6e7c83bfb9
Noah Shan <lust595@gmail.com>
2024-02-23 02:01:36 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+const app = require('./app');
+dotenv.config({ path: './config.env' });
+const mongoose = require('mongoose');
+
+const URL = process.env.DB_URL.replace('<PASSWORD>', process.env.DB_PASSWORD);
+console.log(URL);
+mongoose
+  .connect(URL, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then((con) => {
+    console.log('Connected to database!');
+  })
+  .catch((err) => console.error(err));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, (req, res) => {
+  console.log(`listening on port: ${port}`);
+});
